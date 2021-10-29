990be10b74cb48d91c335d6262e758b595c70f26
陳育霖 Leo <75311991+wendy062644@users.noreply.github.com>
2021-10-29 16:14:01 +0800

    Create a491: 貨物集中問題.cpp

---
+++ a491: 貨物集中問題.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	while(n--)
+	{
+		vector<int> x(4000001);
+		vector<int> y(4000001);
+		vector<int> p(4000001);
+		long long R, C, point, num, N = 0, min, X, Y;
+		cin >> R >> C;
+		for(int a = 1; R >= a; a++)
+		{
+			for(int b = 1; C >= b; b++)
+			{
+				cin >> num;
+				if(num > 0){x[N] = b; y[N] = a; p[N] = num; N++;}
+			}
+		}
+		for(int a = 1; R >= a; a++)
+		{
+			for(int b = 1; C >= b; b++)
+			{
+				point = 0;
+				for(int c = 0; N > c; c++)
+				{
+					point = point + p[c]*(abs(x[c]-b)+abs(y[c]-a));
+				}
+				if(a == 1&&b == 1){min = point; X = a; Y = b;}
+				if(point < min){min = point; X = a; Y = b;}
+			}
+		}
+		cout << X << " " << Y << endl << min*100 << endl;
+	}
+} 
