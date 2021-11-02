9c815c589af56a69a865d9afab68ba51fab97d31
Leo <75311991+wendy062644@users.noreply.github.com>
2021-11-02 13:37:39 +0800

    Update a491: 貨物集中問題.cpp

--- a491: 貨物集中問題.cpp
+++ a491: 貨物集中問題.cpp
@@ -2,37 +2,48 @@
 using namespace std;
 int main()
 {
-	int n, X, Y, R, C;
-	long long min, point;
+	int n, R, C, num;
+	long long minx, miny, point, xx, yy;
 	cin >> n;
 	while(n--)
 	{
-		vector<int> x(4000001);
-		vector<int> y(4000001);
-		vector<int> p(4000001);
+		vector<long long> x(2001);
+		vector<long long> y(2001);
+		vector<long long> X(2001);
+		vector<long long> Y(2001);
 		cin >> R >> C;
 		for(int a = 1; R >= a; a++)
 		{
 			for(int b = 1; C >= b; b++)
 			{
-				cin >> p[(a-1)*C+b-1];
-				x[(a-1)*C+b-1] = b;
-				y[(a-1)*C+b-1] = a;
+				cin >> num;
+				x[b] += num;
+				y[a] += num;
+			}
+		}
+		for(int a = 1; C >= a; a++)
+		{
+			for(int b = 1; C >= b; b++)
+			{
+				X[a] += x[b]*(abs(a-b));
+			}
+			if(a == 1){minx = X[a]; xx = a;}
+			else{
+				if(minx > X[a]){minx = X[a]; xx = a;}
 			}
 		}
 		for(int a = 1; R >= a; a++)
 		{
-			for(int b = 1; C >= b; b++)
+			for(int b = 1; R >= b; b++)
 			{
-				point = 0;
-				for(int c = 0; R*C > c; c++)
-				{
-					point = point + p[c]*(abs(x[c]-b)+abs(y[c]-a));
-				}
-				if(a == 1&&b == 1){min = point; X = a; Y = b;}
-				if(point < min){min = point; X = a; Y = b;}
+				Y[a] += y[b]*(abs(a-b));
+			}
+			if(a == 1){miny = Y[a]; yy = a;}
+			else{
+				if(miny > Y[a]){miny = Y[a]; yy = a;}
 			}
 		}
-		cout << X << " " << Y << endl << min*100 << endl;
+		cout << yy << " " << xx << endl << (minx+miny)*100 << endl;
 	}
 } 
+//題目: https://zerojudge.tw/ShowProblem?problemid=a491  NA (score:80%)
